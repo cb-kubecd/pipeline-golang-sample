b37b036b28445496d7493cb7a5ae88dd1e5d0a67
rawlingsj <rawlingsj80@gmail.com>
2019-01-29 19:27:22 +0000

    update readme

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example - test 4"
+	title := "Jenkins X golang http example - test 5"
 
 	from := ""
 	if r.URL != nil {
